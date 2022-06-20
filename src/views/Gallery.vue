9cf8dadb0a56cae0d8ed13654cee1f18a2559d81
matt-harding-2021 <09maharding@gmail.com>
2022-06-20 23:56:11 +0100

    gallery branch created

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,6 +1,24 @@
 <template>
   <View :media="media">
-    <p>GALLERY</p>
+    <div class='
+      flex-1 flex flex-wrap
+      p-8 gap-8
+    '>
+      <!-- tailwind doesn't include classes of dynamic elements -->
+      <div v-for="item in items" :key="item.id"
+      class='
+        flex-0
+      '>
+        <!-- tailwind does include classes of the defined elements within -->
+        <div class='
+          basis-1/3
+          h-64
+          border
+          bg-red-600
+        '>
+        </div>
+      </div>
+    </div>
   </View>
 </template>
 
@@ -17,6 +35,12 @@ import View from '@/components/global/View.vue';
   },
   data() {
     return {
+      items: [
+        {},
+        {},
+        {},
+        {},
+      ]
     };
   },
   computed: {
