95eb444bb4960d063cf76635fdb6525aa9eabbaa
matt-harding-2021 <09maharding@gmail.com>
2022-10-31 10:58:35 +0000

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <View :nav="false" :media="media">
+    <button class="absolute z-10"
+    :ref="(x) => {testSetRef(x)}"
+    :onClick="testGetRef">console.log</button><!-- targetSlide -->
     <div :class="`
-      ${currentSlide === 1 ? '' : '-top-full opacity-0'}
+      ${
+        currentSlide < 1 ? 'top-full opacity-0' :
+        currentSlide > 1 ? '-top-full opacity-0' :
+        ''
+      }
       absolute inset-0
       flex flex-col
+      h-full
       justify-center items-center
       gap-16
-      transition-all duration-1000
+      transition-all duration-800
     `">
       <h1 class='text-2xl tracking-0.15'> Hi, I'm Matthew Harding </h1>
       <div class='
@@ -20,38 +28,150 @@
         <div class='arrow-right w-3 h-3' />
       </div>
     </div>
+
     <div :class="`
-      ${currentSlide === 2 ? '' : 'top-full opacity-0'}
+      ${
+        currentSlide < 2 ? 'top-full opacity-0' :
+        currentSlide > 2 ? '-top-full opacity-0' :
+        ''
+      }
       absolute inset-0
       flex flex-col
+      h-full
       justify-center items-center
       gap-16
-      transition-all duration-1000
+      transition-all duration-800
     `">
       <h2 class='text-lg tracking-0.2'> Frontend Developer </h2>
       <div class='
         flex
         items-center
-        gap-4
-        cursor-pointer
+        gap-12
       '
       :onScroll="(e) => scrollFunction(e)">
-        <p class='flex-1'>Lorem</p>
+        <p class='flex-1 w-96'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique eros vitae vehicula commodo.
+          <br />
+          Donec facilisis sed quam vel rutrum.
+          <br />
+          Duis efficitur dui lectus. In hac habitasse platea dictumst. 
+        </p>
         <div class='
-          flex-1
-          h-48 w-64
-          rounded-lg
-          glass
+          flex flex-wrap
+          w-96
+          justify-center
+          p-12 gap-12
           transition-all duration-500
-        ' />
+          glass
+        '>
+          <a href="https://www.figma.com/about/" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoFigma' />
+          </a>
+          <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoJS' />
+          </a>
+          <a href="https://www.solidjs.com/" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoSolid' />
+          </a>
+          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoTailwind' />
+          </a>
+          <a href="https://vuejs.org/" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoVue' />
+          </a>
+        </div>
       </div>
     </div>
+
+    
+    <div :class="`
+      ${
+        currentSlide < 3 ? 'top-full opacity-0' :
+        currentSlide > 3 ? '-top-full opacity-0' :
+        ''
+      }
+      absolute inset-0
+      flex flex-col
+      h-full
+      justify-center items-center
+      gap-16
+      transition-all duration-800
+    `">
+      <h2 class='text-lg tracking-0.2'> Current Work </h2>
+      <div class='
+        flex
+        items-center
+        gap-12
+      '
+      :onScroll="(e) => scrollFunction(e)">
+        <p class='flex-1 w-96'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique eros vitae vehicula commodo.
+          <br />
+          Donec facilisis sed quam vel rutrum.
+          <br />
+          Duis efficitur dui lectus. In hac habitasse platea dictumst. 
+        </p>
+        <div class='
+          flex flex-wrap
+          w-96
+          justify-center
+          p-12 gap-12
+          transition-all duration-500
+          glass
+        '>
+          <a href="https://www.finlight.com/" target="_blank" rel="noreferrer noopener">
+            <img class='w-12 h-12' :src='logoFigma' />
+          </a>
+        </div>
+      </div>
+    </div>
+    
+    <div :class="`
+      ${
+        currentSlide < 4 ? 'top-full opacity-0' :
+        currentSlide > 4 ? '-top-full opacity-0' :
+        ''
+      }
+      absolute inset-0
+      flex flex-col
+      h-full
+      justify-center items-center
+      gap-16
+      transition-all duration-800
+    `">
+      <h2 class='text-lg tracking-0.2'> Test </h2>
+      <div class='
+        flex
+        items-center
+        gap-12
+      '
+      :onScroll="(e) => scrollFunction(e)">
+        <p class='flex-1 w-96'>
+          Donec facilisis sed quam vel rutrum.
+        </p>
+        <div class='
+          flex flex-wrap
+          w-96
+          justify-center
+          p-12 gap-12
+          transition-all duration-500
+          glass
+        '>
+        </div>
+      </div>
+    </div>
+
   </View>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import View from '@/components/global/View.vue';
+import logoFigma from "@/assets/images/logos/figma_icon.svg";
+import logoJS from "@/assets/images/logos/javascript_icon.svg";
+import logoSolid from "@/assets/images/logos/solidjs_icon.svg";
+import logoTailwind from "@/assets/images/logos/tailwind_icon.svg";
+import logoVue from "@/assets/images/logos/vue-js_icon.svg";
 
 @Options({
   components: {
@@ -62,26 +182,88 @@ import View from '@/components/global/View.vue';
   },
   data() {
     return {
-      currentSlide: 1
+      logoFigma,
+      logoJS,
+      logoSolid,
+      logoTailwind,
+      logoVue,
+
+      currentSlide: 1,
+      isAnimating: false,
+
+      refTest: null,
+
+      xDown: 0,
+      yDown: 0
     };
   },
   methods: {
+    testSetRef: function(x: null) {
+      this.refTest=x;
+    },
+    testGetRef: function() {
+      /* console.log(this.refTest) */
+      console.log(this.currentSlide)
+    },
     changeSlide(slide: number) {
       let vm = this;
-      vm.currentSlide = 0;
-      setTimeout(() => {
-        vm.currentSlide = slide
-      }, 500);
+      if(!vm.isAnimating && !(slide<0 || slide>4)) {
+        vm.isAnimating = true;
+        /* vm.currentSlide = 0; */
+        vm.currentSlide = slide;
+        /* setTimeout(() => { */
+          /* vm.currentSlide = vm.targetSlide; */
+          setTimeout(() => {
+            vm.isAnimating = false;
+          }, 800);
+        /* }, 800); */
+      }
     },
-    scrollFunction(e: object) {
-      let vm = this;1
-      vm.currentSlide += Math.sign(e.deltaY);
-      console.log(vm.currentSlide)
-    },
-    init() {
+    scrollFunction(e: {deltaY: 0}) {
       let vm = this;
-      window.addEventListener('mousewheel', (e) => vm.scrollFunction(e));
-      //removeEventListener(type, listener);
+      console.log(e)
+      vm.changeSlide(vm.currentSlide + Math.sign(e.deltaY))
+    },
+
+
+    getTouches(evt: any) {
+      return evt.touches ||// browser API
+            evt.originalEvent.touches; // jQuery
+    },
+
+    handleTouchStart(evt: any) {
+        const firstTouch = this.getTouches(evt)[0];
+        this.xDown = firstTouch.clientX;
+        this.yDown = firstTouch.clientY;
+    },
+
+    handleTouchMove(evt: any) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = this.xDown - xUp;
+        var yDiff = this.yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 0 ) {
+                /* right swipe */ 
+            } else {
+                /* left swipe */
+            }
+        } else {
+            if ( yDiff > 0 ) {
+              this.changeSlide(this.currentSlide + Math.sign(yDiff))
+            } else { 
+              this.changeSlide(this.currentSlide + Math.sign(yDiff))
+            }
+        }
+        /* reset values */
+        this.xDown = null;
+        this.yDown = null;
     }
   },
   watch: {
@@ -93,7 +275,15 @@ import View from '@/components/global/View.vue';
     } */
   },
   mounted() {
-    this.init()
+    let vm = this;
+    window.addEventListener('mousewheel', (e) => vm.scrollFunction(e));
+    /* window.addEventListener('touchmove', (e) => vm.scrollFunction(e)); */
+    window.addEventListener('touchstart', vm.handleTouchStart, false);        
+    window.addEventListener('touchmove', vm.handleTouchMove, false);
+  },
+  unmount() {
+    let vm = this;
+    window.removeEventListener('mousewheel', (e) => vm.scrollFunction(e));
   }
 })
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        test: '200vh'
+      },
       letterSpacing: {
         0.15: '0.15em',
         0.2: '0.2em'
+      },
+      transitionDuration: {
+        '800': '800ms',
       }
     },
   },
